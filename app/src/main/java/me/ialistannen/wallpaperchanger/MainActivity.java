46c6cd7c75d37f2e8ccb9bead18be70a045dbe66
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2017-07-18 14:17:04 +0200

    Working on the auto-changer

--- app/src/main/java/me/ialistannen/wallpaperchanger/MainActivity.java
+++ app/src/main/java/me/ialistannen/wallpaperchanger/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import me.ialistannen.wallpaperchanger.automaticchanging.WallpaperChangerTask;
 import me.ialistannen.wallpaperchanger.images.provider.ImageProvider;
 import me.ialistannen.wallpaperchanger.images.provider.ProviderFactory;
 import me.ialistannen.wallpaperchanger.images.util.RandomImageObtainTask;
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     if (savedInstanceState == null) {
       setSupportActionBar((Toolbar) findViewById(R.id.activity_main_actionbar));
+
+      new WallpaperChangerTask().registerIfEnabled(this);
     }
 
     if (retainFragment == null) {
